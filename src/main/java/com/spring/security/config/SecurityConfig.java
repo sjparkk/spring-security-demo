7deb64df40cbe51021d393b7fc87f2630d2ac0bc
sjparkk <sjparkk0820@naver.com>
2021-11-15 23:23:11 +0900

    feat : http 관련 인증 설정

--- src/main/java/com/spring/security/config/SecurityConfig.java
+++ src/main/java/com/spring/security/config/SecurityConfig.java
@@ -17,4 +17,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/h2-console/**"); // h2 콘솔도 추가해줌
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception { // http 관련 인증 설정
+        http
+                .authorizeRequests() // 접근에 대한 인증 설정
+                .antMatchers("/login", "/signup", "/user").permitAll() // 누구나 접근 허용
+                .antMatchers("/").hasRole("USER") // USER, ADMIN만 접근 가능
+                .antMatchers("/admin").hasRole("ADMIN") // ADMIN만 접근 가능
+                .anyRequest().authenticated() // 나머지 요청들은 권한의 종류에 상관 없이 권한이 있어야 접근 가능
+                .and()
+                .formLogin() // 로그인에 관한 설정
+                .loginPage("/login") // 로그인 페이지 링크
+                .defaultSuccessUrl("/") // 로그인 성공 후 리다이렉트 주소
+                .and()
+                .logout() // 로그아웃
+                .logoutSuccessUrl("/login") // 로그아웃 성공시 리다이렉트 주소
+                .invalidateHttpSession(true) // 세션 날리기
+        ;
+    }
 }
