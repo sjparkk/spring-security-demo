6dc0c94072ed4af3b61bcf7d5d5ad6858148cff8
sjparkk <sjparkk0820@naver.com>
2021-11-15 23:46:42 +0900

    feat : 회원 가입시 회원 정보 저장 및 회원 Pk 반환 해주는 save 메서드 추가

--- src/main/java/com/spring/security/service/UserService.java
+++ src/main/java/com/spring/security/service/UserService.java
@@ -1,10 +1,12 @@
 package com.spring.security.service;
 
 import com.spring.security.domain.User;
+import com.spring.security.dto.UserDTO;
 import com.spring.security.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -17,5 +19,21 @@ public class UserService implements UserDetailsService {
     public User loadUserByUsername(String email) throws UsernameNotFoundException {
         return userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException((email)));
+    }
+
+    /**
+     * 회원정보 저장
+     *
+     * @param userDto 회원정보가 들어있는 DTO
+     * @return 저장되는 회원의 PK
+     */
+    public Long save(UserDTO userDto) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        userDto.setPassword(encoder.encode(userDto.getPassword()));
+
+        return userRepository.save(User.builder()
+                .email(userDto.getEmail())
+                .auth(userDto.getAuth())
+                .password(userDto.getPassword()).build()).getCode();
     }
 }
