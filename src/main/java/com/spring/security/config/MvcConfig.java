04384b463af62a46294b83fe4e6d5210db6170fc
sjparkk <sjparkk0820@naver.com>
2021-11-16 23:32:33 +0900

    feat : 뷰 연결 config 기능 추가

---
+++ src/main/java/com/spring/security/config/MvcConfig.java
@@ -0,0 +1,17 @@
+package com.spring.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    //컨트롤러 클래스 없이 특정 view에 대한 컨트롤러 추가
+    public void addViewController(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("main");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/admin").setViewName("admin");
+        registry.addViewController("/signup").setViewName("signup");
+    }
+}
