0b56ca2b330f6ac8db4f20a1d086a3204bf6c588
sjparkk <sjparkk0820@naver.com>
2021-11-16 23:21:56 +0900

    feat : 로그아웃 추가

--- src/main/java/com/spring/security/controller/UserController.java
+++ src/main/java/com/spring/security/controller/UserController.java
@@ -3,8 +3,15 @@ package com.spring.security.controller;
 import com.spring.security.dto.UserDTO;
 import com.spring.security.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RequiredArgsConstructor
 @Controller
@@ -14,6 +21,15 @@ public class UserController {
     @PostMapping("/user")
     public String signup(UserDTO userDTO) {
         userService.save(userDTO);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
         return "redirect:/login";
     }
 }
