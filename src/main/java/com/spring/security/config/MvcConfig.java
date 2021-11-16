4615a3befea7bd4e50df400c9075e496316076a6
sjparkk <sjparkk0820@naver.com>
2021-11-16 23:54:59 +0900

    fix : 메서드명 오타 수정

--- src/main/java/com/spring/security/config/MvcConfig.java
+++ src/main/java/com/spring/security/config/MvcConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 
     //컨트롤러 클래스 없이 특정 view에 대한 컨트롤러 추가
-    public void addViewController(ViewControllerRegistry registry) {
+    public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("main");
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/admin").setViewName("admin");
